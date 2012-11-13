6543d4f8334c36df6cfa4b8b1e39c7c0a09030e9
Jitendra <jitendra.takalkar@gmail.com>
2012-11-13 23:00:50 +0530

    Easy: 146

--- src/four_cljr/core.clj
+++ src/four_cljr/core.clj
@@ -40,3 +40,64 @@
     (cond (<= n 1) (reverse acc)
           (zero? (rem n candidate)) (recur (/ n candidate) candidate (cons candidate acc))
           :else (recur n (inc candidate) acc))))
+
+(defn visit
+  [node]
+  (println node))
+
+(defn inorder
+  [node]
+  (if-not (coll? node)        
+    (visit node)
+    (do (inorder (second node)) 
+      (visit (first node)) 
+      (inorder (nth node 2)))))
+(defn in-order
+  ([node] (in-order node (atom [])))
+  ([node rs]
+     (if-not (coll? node)
+       (swap! rs conj node)
+       (do (in-order (second node) rs)
+           (swap! rs conj (first node))
+           (in-order (nth node 2) rs)))))
+
+;;(inorder [:a [:b 1 2] [:b 2 1]])
+
+;;boolean mirrorEquals(BTree left, BTree right) {
+;;  if (left == null || right == null) return left == null && right == null;
+;;  return left.value == right.value && mirrorEquals(left.left, right.right) && mirrorEquals(left.right, right.left);
+;;}
+
+(defn mirror-equals?
+  [left right]
+  (if (or (nil? left) (nil? right))
+    (and (nil? left) (nil? right))
+    (and (= (if (coll? left) (first left) left) (if (coll? right) (first right) right))
+         (mirror-equals? (second left) (nth right 2))
+         (mirror-equals? (nth left 2) (second right)))))
+
+(defn preorder
+  [node]
+  (if-not (coll? node)
+    (visit node)
+    (do (visit (first node))
+      (preorder (second node))
+      (preorder (nth node 2)))))
+
+;; (preorder [:a [:b 1 2] [:b 2 1]])
+
+(defn postorder
+  [node]
+  (if-not (coll? node)
+    (visit node)
+    (do (postorder (second node))
+      (postorder (nth node 2))
+      (visit (first node)))))
+
+;;(postorder [:a [:b 1 2] [:b 2 1]])
+
+(defn tree-into-tables
+  [m]
+  (apply merge         
+         (for [x m y (second x)]
+           {[(first x) (first y)] (second y)})))

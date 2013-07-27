30ae144dfea8282b1a76343ea69ad6d07ce26200
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2013-07-27 21:00:50 +0530

    problem#54

--- test/four_cljr/medium_test.clj
+++ test/four_cljr/medium_test.clj
@@ -64,6 +64,22 @@
     (is (= (set (split-by-type [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]}))
     (is (= (set (split-by-type [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]}))))
 
+;; 54
+;; Medium
+;; seqs core-functions
+;; Write a function which returns a sequence of lists of x items each. Lists of less than x items should not be returned.
+;; Special Restrictions - partition - partition-all
+
+(defn partition-seq [n s]
+  (if (>= (count s) n)
+    (cons (take n s) (partition-seq n (drop n s)))))
+
+(deftest test-54
+  (testing "Partition a Sequence"
+    (is (= (partition-seq 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8))))
+    (is (= (partition-seq 2 (range 8)) '((0 1) (2 3) (4 5) (6 7))))
+    (is (= (partition-seq 3 (range 8)) '((0 1 2) (3 4 5))))))
+
 ;; 55
 ;; Medium
 ;; seqs core-functions

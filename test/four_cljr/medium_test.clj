3df7fa26db093b1c2bb8140cd1156aa6bffe6abe
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2013-07-28 19:14:46 +0530

    problem#98

--- test/four_cljr/medium_test.clj
+++ test/four_cljr/medium_test.clj
@@ -397,6 +397,26 @@
     (= (happy-number 2) false)
     (= (happy-number 3) false)))
 
+;; 98
+;; Medium
+;; A function f defined on a domain D induces an equivalence relation on D, as follows: a is equivalent to b with respect to f if and only if (f a) is equal to (f b). Write a function with arguments f and D that computes the equivalence classes of D with respect to f.
+(defn equi-classes
+  [f s]
+  (clojure.walk/walk #(into #{} %)
+                     #(into #{} %)
+                     (vals (group-by f s))))
+
+(deftest test-98
+  (testing "Equivalence Classes"
+    (is (= (equi-classes #(* % %) #{-2 -1 0 1 2})
+           #{#{0} #{1 -1} #{2 -2}}))
+    (is (= (equi-classes #(rem % 3) #{0 1 2 3 4 5 })
+           #{#{0 3} #{1 4} #{2 5}}))
+    (is (= (equi-classes identity #{0 1 2 3 4})
+           #{#{0} #{1} #{2} #{3} #{4}}))
+    (is (= (equi-classes (constantly true) #{0 1 2 3 4})
+           #{#{0 1 2 3 4}}))))
+
 ;; 102
 ;; Medium
 ;; strings

d423c12483cf9431f084565be48a0c95b7394f12
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2013-08-03 17:14:58 +0530

    problem#137

--- test/four_cljr/medium_test.clj
+++ test/four_cljr/medium_test.clj
@@ -546,3 +546,25 @@
     (= false (prime-sandwich 4))
     (= true (prime-sandwich 563))
     (= 1103 (nth (filter prime-sandwich (range)) 15))))
+
+;; 137
+;; Medium
+;; math
+;; Write a function which returns a sequence of digits of a non-negative number (first argument) in numerical system with an arbitrary base (second argument). Digits should be represented with their integer values, e.g. 15 would be [1 5] in base 10, [1 1 1 1] in base 2 and [15] in base 16.
+
+(defn to-base
+  [d b]
+  (loop [id d
+         res nil]
+    (if (zero? id)
+      (or res [0])
+      (recur (quot id b)
+             (cons (rem id b) res)))))
+
+(deftest test-137
+  (testing "Digits and bases"
+    (is (= [1 2 3 4 5 0 1] (to-base 1234501 10)))
+    (is (= [0] (to-base 0 11)))
+    (is (= [1 0 0 1] (to-base 9 2)))
+    (is (= [1 0] (let [n (rand-int 100000)](to-base n n))))
+    (is (= [16 18 5 24 15 1] (to-base Integer/MAX_VALUE 42)))))
